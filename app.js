76f7add40d7c030a2bbac9fda9b41ee5b73546a1
Alok Kumar <aloknh31@gmail.com>
2024-12-24 20:58:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express =  require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+	res.send('hi hello')
+})
+
+const PORT = 3000
+
+app.listen(PORT,()=>{
+console.log(`Server is listening at http://localhost:${PORT}`)
+})
+
+
